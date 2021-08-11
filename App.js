7e4cf6af3a092de6cae32f6848dc612f1989d6d0
dmarins <diogodsfmarins@gmail.com>
2021-08-11 16:28:02 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{color: '#c2c2c2'}}>Olá, mundo!</Text>
+    </View>
+  );
+};
+
+export default App;
